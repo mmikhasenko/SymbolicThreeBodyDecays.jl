e12c571f45ff8c059ca6cb641800ccb2ae36d798
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2023-11-14 10:46:29 +0100

    fixed threebodydecay consistency

--- src/dispatch.jl
+++ src/dispatch.jl
@@ -21,23 +21,23 @@ end
 label(wr::TriavialWignerRotation) = "^k_i(i)"
 
 function label(wr::WignerRotation{0})
-    i,j,k = ijk(wr)
+    i, j, k = ijk(wr)
     "^0_" * (ispositive(wr) ? "$(k)($(i))" : "$(i)($(k))")
 end
 function label(wr::WignerRotation{3})
-    i,j,k = ijk(wr)
+    i, j, k = ijk(wr)
     "^$(k)_" * (ispositive(wr) ? "$(i)($(j))" : "$(j)($(i))")
 end
 function label(wr::WignerRotation{2})
-    i,j,k = ijk(wr)
+    i, j, k = ijk(wr)
     w = (iseven(wr) ? i : j)
-    return "^$(k)_" * (ispositive(wr)==iseven(wr) ? "$(w)($(k))" : "$(k)($(w))")
+    return "^$(k)_" * (ispositive(wr) == iseven(wr) ? "$(w)($(k))" : "$(k)($(w))")
 end
 #
 for N in (0, 2, 3)
     eval(:(
         function cosζ(wr::WignerRotation{$(N)},
-            σs::StickySymTuple{(:σ1, :σ2, :σ3), 3}, msq)
+            σs::StickySymTuple{(:σ1, :σ2, :σ3),3}, msq)
             ζ = sympy.Symbol("ζ" * label(wr), real=true)
             return cosHold(ζ, cosζ(wr, getfield(σs, :data), msq))
         end
@@ -45,7 +45,7 @@ for N in (0, 2, 3)
 end
 
 # redefine wignerd_doublearg
-function wignerd_doublearg_sign(two_j, two_λ1, two_λ2, cosθ::cosHold, ispositive)
+function wignerd_doublearg_sign(two_j, two_λ1, two_λ2, cosθ::cosHold, ispositive::Bool)
     half = 1 / Sym(2)
     (abs(two_λ1) > two_j || abs(two_λ2) > two_j) && return zero(cosθ.angle)
     return WignerD(two_j * half, two_λ1 * half, two_λ2 * half,
